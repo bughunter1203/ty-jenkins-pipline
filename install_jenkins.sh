e10dc98514dfaa4e2958dce4ef8c1a5e84f22ff0
bughunter1203 <78723407+bughunter1203@users.noreply.github.com>
2024-10-13 13:30:18 +0900

    초기화

---
+++ install_jenkins.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# this script is only tested on ubuntu focal 20.04 (LTS)
+
+# install docker
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker $USER
+echo "Please log out and log back in to apply Docker group changes."
+
+# run jenkins
+mkdir -p /var/jenkins_home
+sudo chown -R 1000:1000 /var/jenkins_home/
+docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
+
+# show endpoint and initial admin password
+echo "Jenkins installed. You can access it at: http://$(curl -4 -s ifconfig.co):8080"
+echo "Jenkins initial admin password:"
+docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword
